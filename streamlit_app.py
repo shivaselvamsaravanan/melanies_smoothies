c988fde30f244993101a0eb9ed57eaac46044628
shivaselvamsaravanan <76870389+shivaselvamsaravanan@users.noreply.github.com>
2025-06-29 09:52:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title(f":cup_with_straw: Customize your smoothie :cup_with_straw: ")
@@ -26,3 +25,5 @@ if ingredient_list:
     if submit_:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered {name}!", icon="✅")
+cnx = st.connection("snowflake")
+session=cnx.session();
